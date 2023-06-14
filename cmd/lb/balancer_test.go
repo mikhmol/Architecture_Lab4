c2ed8d735a6a8c4c177cbf0001163eb7c748c029
mikhmol <mikhailo.moliakov@gmail.com>
2023-06-15 00:32:51 +0300

    Added balancer test

--- cmd/lb/balancer_test.go
+++ cmd/lb/balancer_test.go
@@ -1,7 +1,30 @@
 package main
 
-import "testing"
+import (
+	"testing"
 
-func TestBalancer(t *testing.T) {
-	// TODO: Реалізуйте юніт-тест для балансувальникка.
+	check "gopkg.in/check.v1"
+)
+
+// func TestBalancer(t *testing.T) {
+// 	// TODO: Реалізуйте юніт-тест для балансувальникка.
+// }
+
+func Test(t *testing.T) { check.TestingT(t) }
+
+type MySuite struct{}
+
+var _ = check.Suite(&MySuite{})
+
+func (s *MySuite) TestGetMinByteServer(c *check.C) {
+	// Given
+	serverBytes["server1:8080"] = 500
+	serverBytes["server2:8080"] = 200
+	serverBytes["server3:8080"] = 300
+
+	// When
+	minServer := getMinByteServer()
+
+	// Then
+	c.Assert(minServer, check.Equals, "server2:8080")
 }
