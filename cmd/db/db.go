ab59876fdd9610e9298ade6769cb4ad9ef91adbc
mikhmol <mikhailo.moliakov@gmail.com>
2023-06-16 10:19:50 +0300

    Add database server

---
+++ cmd/db/db.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/mikhmol/Architecture_Lab4/datastore"
+	"github.com/mikhmol/Architecture_Lab4/httptools"
+	"github.com/mikhmol/Architecture_Lab4/signal"
+)
+
+var port = flag.Int("port", 8080, "server port")
+
+type Request struct {
+	Value string `json:"value"`
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	dir, err := ioutil.TempDir("", "test-db")
+	if err != nil {
+		fmt.Println("Error creating temporary directory:", err)
+		os.Exit(1) // Exit with a non-zero error code
+	}
+	defer func() {
+		os.RemoveAll(dir)
+	}()
+
+	db, err := datastore.NewDb(dir)
+	if err != nil {
+		fmt.Println("Error creating database:", err)
+		os.Exit(1) // Exit with a non-zero error code
+	}
+	defer db.Close()
+
+	r.HandleFunc("/db/{key}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		key := vars["key"]
+
+		value, err := db.Get(key)
+		if err != nil {
+			if err == datastore.ErrNotFound {
+				http.NotFound(w, r)
+			} else {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+			}
+			return
+		}
+
+		response := struct {
+			Key   string      `json:"key"`
+			Value interface{} `json:"value"`
+		}{
+			Key:   key,
+			Value: value,
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		if err := json.NewEncoder(w).Encode(response); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}).Methods("GET")
+
+	r.HandleFunc("/db/{key}", func(w http.ResponseWriter, r *http.Request) {
+		var request Request
+		vars := mux.Vars(r)
+		key := vars["key"]
+
+		err := json.NewDecoder(r.Body).Decode(&request)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		err = db.Put(key, request.Value)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		w.Header().Set("content-type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		_ = json.NewEncoder(w).Encode(request)
+	}).Methods("POST")
+
+	server := httptools.CreateServer(*port, r)
+	server.Start()
+	signal.WaitForTerminationSignal()
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/mikhmol/Architecture_Lab4
 go 1.20
 
 require (
+	github.com/gorilla/mux v1.8.0
 	github.com/kr/pretty v0.2.1 // indirect
 	github.com/kr/text v0.1.0 // indirect
 	golang.org/x/sync v0.3.0
